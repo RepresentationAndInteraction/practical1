bb6f6fe5abc19a2e340c8a305b9bc984a9a10c66
Hendrikto <hendrik.to@gmail.com>
2017-10-02 15:54:15 +0200

    Define a rule to print trees.

--- practical.pl
+++ practical.pl
@@ -6,3 +6,12 @@ node(1, alex, [3]).
 node(2, conny, [4]).
 node(3, mauro, []).
 node(4, maurice, []).
+
+% print_tree(root_id)
+print_tree(Root_id) :-
+	node(Root_id, Content, Children),
+	write("Node "), write(Root_id), nl,
+	write("\tContent: "), write(Content), nl,
+	!,
+	member(Next_node, Children),
+	print_tree(Next_node).
